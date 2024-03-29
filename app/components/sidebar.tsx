0c82e9de498b7e660a9751c648bc42ec1f1a089b
lxc00215 <1677507428@qq.com>
2024-03-29 09:17:26 +0800

    更改了编码问题

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           NextChat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          API_KEYSÓÉ<a href="https://api.leebay.asia">¡¶½ğÊ¯AI¡·</a>Ìá¹©
+          API_KEYSç”±<a href="https://api.leebay.asia">ã€Šé‡‘çŸ³AIã€‹</a>æä¾›
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
